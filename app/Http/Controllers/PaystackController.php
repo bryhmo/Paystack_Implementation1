35a8d3d9b5cd0a9dbd1d6d1705debe2503463f4c
bryhmo <codewithbryhmo@gmail.com>
2023-06-29 21:02:17 +0100

    CURL properties inserted into the payment

--- app/Http/Controllers/PaystackController.php
+++ app/Http/Controllers/PaystackController.php
@@ -13,23 +13,31 @@ class PaystackController extends Controller
     }
     public function propayment()
     {
-        return view('mypayment.pricefrom');
+      return view('mypayment.pricefrom');
     }
 
     public function responsedata()
     {
 
         return view('mypayment.testdata');
-    //     $formData =array([
-    //         'email'=>request('email'),
-    //          'amount'=>request('amount'),
-
-    //     ]);
+        $formData =array([
+            'email'=>request('email'),
+            'amount'=>request('amount'),
+        ]);
     }
     public function initiate_payment()
     {
-        //  $url ="https://api.paystack.co/transaction/initialize";
-        // $ch = curl_init();
+         $url ="https://api.paystack.co/transaction/initialize";
+         //this is the customer url that will be generated for payment initialization
+         $ch = curl_init();
+         curl_setopt($ch,CURLOPT_URL,$url);
+         curl_setopt($ch,CURLOPT_POST,true);
+         curl_setopt($ch,CURLOPT_FIELDS,$fields_string);
+         curl_setopt($ch,CURLOPT_HTTPHEADER, array(
+            [
+            'Authorization:Bearer',
+            'cache-control:no-cache',
+         ]));
 
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 
 class UsersTableSeeder extends Seeder
 {
